88b7e4abae2a4984c43b751e1fb62fb962161d88
mugiwarapose <mugiwarapose@gmail.com>
2016-05-29 08:48:47 +0800

    simple reflect samole

---
+++ src/com/pose/po/Person.java
@@ -0,0 +1,43 @@
+package com.pose.po;
+
+import java.util.Date;
+
+public class Person {
+	private String id;
+	private String name;
+	private String address;
+	private int age;
+	private Date birthday;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+}
---
+++ src/com/pose/util/SetPorperties.java
@@ -0,0 +1,9 @@
+package com.pose.util;
+
+import java.util.Map;
+
+public class SetPorperties {
+	public void setPro(Object object,Map<String,Object> params){
+		
+	}
+}
